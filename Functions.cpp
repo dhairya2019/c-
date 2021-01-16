69b2f0e94ca28b1bdfa5ec6d8c1246bdd6b22313
Dhairya <dhairya.chugh77@gmail.com>
2021-01-16 20:10:34 +0530

    Create Functions.cpp

---
+++ Functions.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int max_of_four(int a,int b,int c,int d){
+    int result=0;
+    if(a>b && a>c && a>d){
+        result=a;
+    }
+    else if(b>a && b>c && b>d){
+        result=b;
+    }
+     else if(c>b && c>a && c>d){
+        result=c;
+    }
+     else if(d>b && d>c && d>a){
+        result=d;
+    }
+    else{
+        exit(0);
+    }
+    
+    return result;
+}
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
